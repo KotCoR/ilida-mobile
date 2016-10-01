040c14f65a430bebca84737fa625e1428144bf43
hampos <hampos@me.com>
2016-10-01 16:29:31 +0300

    Initial implementation of accident list view

--- ilida.mobile/ViewModels/AccidentListViewModel.cs
+++ ilida.mobile/ViewModels/AccidentListViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace ilida.mobile
 {
 	public class AccidentListViewModel : BaseViewModel
@@ -8,6 +10,25 @@ namespace ilida.mobile
 		public AccidentListViewModel(INavigationService nav)
 		{
 			_nav = nav;
+			_accidents = new List<Accident>()
+			{
+				new Accident(){ AccidentId="891291", Date="11/09/2016 14:30", Status="Προς Διακανονισμο"},
+				new Accident(){ AccidentId="732291", Date="12/03/2015 11:00", Status="Ολοκληρώθηκε"}
+			};
+		}
+
+		private ICollection<Accident> _accidents;
+		public ICollection<Accident> Accidents
+		{
+			get
+			{
+				return _accidents;
+			}
+			set
+			{
+				_accidents = value;
+				OnPropertyChanged(nameof(Accidents));
+			}
 		}
 	}
 }
